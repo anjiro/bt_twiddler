1cddd3f2b81af89afaf861c30230bd7b6037ca99
Daniel Ashbrook <d.ashbrook@samsung.com>
2013-10-23 16:52:27 -0700

    status lights, reorg

--- bt_twiddler.ino
+++ bt_twiddler.ino
@@ -4,23 +4,41 @@
 #include <usb_keyboard.h>
 #include <i2c_t3.h>
 #include <MAX1704.h>
+#include <SyncLED.h>
 
 #define BT_CMD_PIN 15
 
-#define ROWS 4            //four rows
-#define COLS 3            //three columns
-char keys[ROWS][COLS] =
+unsigned long t  = 0;    //Last time the LED was lit
+unsigned long t2 = 0;    //Last time the battery level was printed
+int b = 1;               //Value of the LED
+char iwrap_string[255];  //Keep track of strings coming from iWrap
+int  str_pos = 0;        //Position in the iwrap string
+unsigned long loopCount;
+String msg;
+MAX1704 fuelGauge;
+int was_cmd_req = 0;   //Set to 1 when a command mode request was sent via BT_CMD_PIN
+unsigned int key_modifier = 0;  //Shift, Alt, etc
+unsigned int chord = 0;  //Keep track of keys pressed
+int is_chord = 0;        //More than one key pressed?
+
+SyncLED StatusLight(13);
+
+//Set up keypad
+#define KEY_ROWS 4
+#define KEY_COLS 3
+char keys[KEY_ROWS][KEY_COLS] =
 {
   {3, 7, 11},
   {2, 6, 10},
   {1, 5,  9},
   {0, 4,  8}
 };
-byte rowPins[ROWS] = { 0, 1, 2, 3 };  //connect to the row pinouts of the kpd
-byte colPins[COLS] = { 4, 5, 6 };     //connect to the column pinouts of the kpd
+byte rowPins[KEY_ROWS] = {0, 1, 2, 3};
+byte colPins[KEY_COLS] = {4, 5, 6};
 unsigned int key_to_key[] = {A, B, C, D, E, F, G, H, SP, TAB, BS, ENT};
-Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, KEY_ROWS, KEY_COLS);
 
+//Set up thumbpad
 #define THUMB_ROWS 1
 #define THUMB_COLS 4
 char thumb_keys[THUMB_ROWS][THUMB_COLS] =
@@ -32,40 +50,24 @@ byte thumbColPins[THUMB_COLS] = {21, 20, 19, 18};
 unsigned int key_to_thumb[] = {NUM, ALT, CTRL, SHIFT};
 Keypad thumb_kpd = Keypad(makeKeymap(thumb_keys), thumbRowPins, thumbColPins, THUMB_ROWS, THUMB_COLS);
 
-unsigned long loopCount;
-String msg;
 
-MAX1704 fuelGauge;
-
-int was_cmd_req = 0;   //Set to 1 when a command mode request was sent via BT_CMD_PIN
 
 void setup()
 {
   Wire.begin(I2C_MASTER, 0, I2C_PINS_16_17, I2C_PULLUP_INT, I2C_RATE_100);
   Serial.begin(9600);
   Serial3.begin(115200);
-  pinMode(13, OUTPUT);
+
+  StatusLight.blinkPattern(0b00110011UL, 50, 8);
   
   fuelGauge.reset();
   fuelGauge.quickStart();
   fuelGauge.showConfig();
   
   //Set up the BT reset pin and reset it
-  
   pinMode(BT_CMD_PIN, OUTPUT);
   //bt_reset();
-  /*digitalWrite(BT_CMD_PIN, HIGH);
-  delay(50);
-  digitalWrite(BT_CMD_PIN, LOW);
-  */
 }
-
-
-unsigned long t = 0;     //Last time the LED was lit
-unsigned long t2 = 0;
-int b = 1;               //Value of the LED
-char iwrap_string[255];  //Keep track of strings coming from iWrap
-int  str_pos = 0;        //Position in the iwrap string
 
 
 void loop()
@@ -76,20 +78,26 @@ void loop()
   unsigned int val = 0;
   int was_chord = 0, i;
   
-  if(millis() - t > 100)
-  {
-    digitalWrite(13, b);
-    b = !b;
-    t = millis();
-  }
-  
+  StatusLight.update();
+
+  //Print the battery charge level every 5 seconds
   if(millis() - t2 > 5000)
   {
-    kprintf("Battery charge: %.2f%%\n", fuelGauge.stateOfCharge());
+    float charge = fuelGauge.stateOfCharge();
+    kprintf("Battery charge: %.2f%%\n", charge);
     t2 = millis();
+
+    if(charge > 75)
+      StatusLight.blinkPattern(0b1111100000000000UL, 100, 8);  //75-100%
+    else if(charge > 50)
+      StatusLight.blinkPattern(0b1010100000000000UL, 100, 8);  //50-75%
+    else if(charge > 25)
+      StatusLight.blinkPattern(0b1010000000000000UL, 100, 8);  //25-50%
+    else
+      StatusLight.blinkPattern(0b1010000000000000UL, 50, 16);   //0-25%
   }
   
-  //Print output from the WT12 module
+  //Print output from the WT12 module and watch for events
   while(Serial3.available())
   {
     c = (char)Serial3.read();
@@ -105,10 +113,11 @@ void loop()
     }
   }
     
-  //Pass input to WT12
+  //Pass input from the serial port to WT12
   while(Serial.available())
     Serial3.print((char)Serial.read());
   
+	//Check for thumb button presses
   if(thumb_kpd.getKeys())
     for(i = 0; i < LIST_MAX; i++)
       if(thumb_kpd.key[i].stateChanged)
@@ -117,6 +126,7 @@ void loop()
         thumbKeypadEvent(key_to_thumb[thumb_kpd.key[i].kchar], thumb_kpd.key[i].kstate);
       }
   
+	//Check for keyboard button presses
   if(kpd.getKeys())
     for(i = 0; i < LIST_MAX; i++)
       if(kpd.key[i].stateChanged)
@@ -124,28 +134,32 @@ void loop()
 }
 
 
-int iwrap_event(char *event)
+//Got a string from the BT module; do something?
+void iwrap_event(char *event)
 {
   if(!strcmp(event, "READY."))
   {
-    Serial.println("Detected 'READY.'");
     if(was_cmd_req)
     {
       was_cmd_req = 0;
       Serial3.println("RESET");
     }
-    return 1;
   }
-   else if(!strcmp(event, "CONNECT 0 HID 11"))
-   {
-     Serial.println("Detected connection; sending SELECT.");
-     Serial3.println("SELECT 0");
-     return 1;
-   }
-   return 0;
+  else if(!strcmp(event, "CONNECT 0 HID 11"))
+    Serial3.println("SELECT 0");
 }
 
 
+//Output the battery level via the keyboard
+void print_battery_level(unsigned int keycode)
+{
+  char battery_level[50];
+  snprintf(battery_level, 50, "\nBattery level %.2f%%\n", fuelGauge.stateOfCharge());
+  send_string(battery_level);
+}
+
+
+//Reset the BT connection
 void bt_reset()
 {
   kprintf("Reset BT chip\n");
@@ -158,12 +172,13 @@ void bt_reset()
 }
 
 
-unsigned int key_modifier = 0;
+//When a thumb key is pressed
 void thumbKeypadEvent(unsigned int kval, int state)
 {
   if(state != PRESSED && state != RELEASED)
     return;
     
+	//BUG: prevents NUM from being used as part of a chord
   if(kval == NUM)
     return keypadEvent(kval, state);
   
@@ -180,23 +195,25 @@ void thumbKeypadEvent(unsigned int kval, int state)
     default:
       break;
   }
-  
-  //char *output_string = get_keymap_string(kval);
-  //kprintf("Modifier: %s ", output_string);
-  if(state == PRESSED)
-    Serial.println("down");
-  else if(state == RELEASED)
-    Serial.println("up");
 }
 
-unsigned int chord = 0;
-int is_chord = 0;
+
+//Called when keypad keys are pressed
+/* 
+	Chord logic: do nothing until a key is released. If it was the only
+	key, print its value. If more than one key was pressed:
+		- if the released key is not the last one (i.e., at least one key is
+			still held down), then it's a chord, so print it out
+		- if the released key was the last one of several, then the chord
+			value has already been printed out, so don't do anything else.
+		- BUG: Press A, B, C, then release C. Print value. Then release B.
+			Nothing should happen, but it will probably print again.
+*/
 void keypadEvent(unsigned int kval, int state)
 {
   if(state != PRESSED && state != RELEASED)
     return;
     
-  //char *keys = get_keymap_key(kval);
   char *output_string;
  
   switch(state)
@@ -219,7 +236,7 @@ void keypadEvent(unsigned int kval, int state)
       else  //More keys held down, so print the current chord
       {
         Serial.print(output_string);
-          send_string(output_string);
+        send_string(output_string);
       }
       break;
 
@@ -228,9 +245,9 @@ void keypadEvent(unsigned int kval, int state)
   }
 }
 
+
 void send_string(char *s)
 {
-  kprintf("send_string(%s)\n", s);
   for(int i = 0; i < strlen(s); i++)
     send_key(s[i]);
 }
@@ -238,8 +255,8 @@ void send_string(char *s)
 
 void send_key(char k)
 {
-  //             0      1     2    3     4     5      6     7    8     9     10    11
-  //                                    mod    0     kb0   kb1   kb2   kb3   kb4   kb5
+  //                0      1     2    3     4     5      6     7    8     9     10    11
+  //                                       mod    0     kb0   kb1   kb2   kb3   kb4   kb5
   uint8_t seq[] = {0x9f, 0x0a, 0xa1, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
   seq[4] = key_modifier;
   seq[6] = convert_char_to_scancode(k);
@@ -254,6 +271,7 @@ void send_key(char k)
   for(int i = 0; i < 12; i++)
     Serial3.write(seq[i]);
   seq[6] = 0;
+  seq[4] ^= SHIFT;
   for(int i = 0; i < 12; i++)
     Serial3.write(seq[i]);
 }
