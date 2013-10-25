2c0c99291bbf2368ac29d7c38cdfc74683727cb7
Daniel Ashbrook <d.ashbrook@samsung.com>
2013-10-25 13:22:40 -0700

    cleanup; fully working (hopefully) multi-chord

--- bt_twiddler.ino
+++ bt_twiddler.ino
@@ -6,27 +6,27 @@
 #include <MAX1704.h>
 #include <SyncLED.h>
 
-#define BT_CMD_PIN 15
+//Periodically do stuff
+MAX1704 fuelGauge;                       // Object for battery charge device
+unsigned long last_battery_print = 0;    //Last time the battery level was printed
+SyncLED StatusLight(13);                 //Blinking status light on pin 13
 
-unsigned long t  = 0;    //Last time the LED was lit
-unsigned long t2 = 0;    //Last time the battery level was printed
-int b = 1;               //Value of the LED
+//Bluetooth communication
+#define BT_CMD_PIN 15    //On transition to high, the WT12 enters command mode
 char iwrap_string[255];  //Keep track of strings coming from iWrap
 int  str_pos = 0;        //Position in the iwrap string
-unsigned long loopCount;
-String msg;
-MAX1704 fuelGauge;
-int was_cmd_req = 0;   //Set to 1 when a command mode request was sent via BT_CMD_PIN
+int was_cmd_req = 0;     //Set to 1 when a command mode request was sent via BT_CMD_PIN
+
+//Keyboard variables
 unsigned int key_modifier = 0;  //Shift, Alt, etc
-unsigned int chord = 0;  //Keep track of keys pressed
-int is_chord = 0;        //More than one key pressed?
+unsigned int keys_down = 0;     //Keep track of keys pressed
+int is_chord = 0;               //More than one key pressed?
+int output_chord = 0;           //Sent keypress for the current chord?
 
-SyncLED StatusLight(13);
-
-//Set up keypad
+//Set up front keypad
 #define KEY_ROWS 4
 #define KEY_COLS 3
-char keys[KEY_ROWS][KEY_COLS] =
+char front_keys[KEY_ROWS][KEY_COLS] =
 {
   {3, 7, 11},
   {2, 6, 10},
@@ -36,7 +36,7 @@ char keys[KEY_ROWS][KEY_COLS] =
 byte rowPins[KEY_ROWS] = {0, 1, 2, 3};
 byte colPins[KEY_COLS] = {4, 5, 6};
 unsigned int key_to_key[] = {A, B, C, D, E, F, G, H, SP, TAB, BS, ENT};
-Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, KEY_ROWS, KEY_COLS);
+Keypad kpd = Keypad(makeKeymap(front_keys), rowPins, colPins, KEY_ROWS, KEY_COLS);
 
 //Set up thumbpad
 #define THUMB_ROWS 1
@@ -70,22 +70,30 @@ void setup()
 }
 
 
+unsigned long t = 0;
+int b = 0;
 void loop()
 {
   char *key;
   char c;
-  unsigned int kval;
+  unsigned int i;
   unsigned int val = 0;
-  int was_chord = 0, i;
   
-  StatusLight.update();
+  //StatusLight.update();
+  /*
+  if(millis() - t > 100)
+  {
+    b = !b;
+    digitalWrite(13, b);
+    t = millis();
+  }*/
 
   //Print the battery charge level every 5 seconds
-  if(millis() - t2 > 5000)
+  if(millis() - last_battery_print > 5000)
   {
     float charge = fuelGauge.stateOfCharge();
     kprintf("Battery charge: %.2f%%\n", charge);
-    t2 = millis();
+    last_battery_print = millis();
 
     if(charge > 75)
       StatusLight.blinkPattern(0b1111100000000000UL, 100, 8);  //75-100%
@@ -94,7 +102,7 @@ void loop()
     else if(charge > 25)
       StatusLight.blinkPattern(0b1010000000000000UL, 100, 8);  //25-50%
     else
-      StatusLight.blinkPattern(0b1010000000000000UL, 50, 16);   //0-25%
+      StatusLight.blinkPattern(0b1010000000000000UL, 50, 16);  // 0-25%
   }
   
   //Print output from the WT12 module and watch for events
@@ -117,7 +125,7 @@ void loop()
   while(Serial.available())
     Serial3.print((char)Serial.read());
   
-	//Check for thumb button presses
+  //Check for thumb button presses
   if(thumb_kpd.getKeys())
     for(i = 0; i < LIST_MAX; i++)
       if(thumb_kpd.key[i].stateChanged)
@@ -126,12 +134,13 @@ void loop()
         thumbKeypadEvent(key_to_thumb[thumb_kpd.key[i].kchar], thumb_kpd.key[i].kstate);
       }
   
-	//Check for keyboard button presses
+  //Check for keyboard button presses
   if(kpd.getKeys())
     for(i = 0; i < LIST_MAX; i++)
       if(kpd.key[i].stateChanged)
         keypadEvent(key_to_key[kpd.key[i].kchar], kpd.key[i].kstate);
 }
+
 
 
 //Got a string from the BT module; do something?
@@ -150,6 +159,7 @@ void iwrap_event(char *event)
 }
 
 
+
 //Output the battery level via the keyboard
 void print_battery_level(unsigned int keycode)
 {
@@ -157,6 +167,7 @@ void print_battery_level(unsigned int keycode)
   snprintf(battery_level, 50, "\nBattery level %.2f%%\n", fuelGauge.stateOfCharge());
   send_string(battery_level);
 }
+
 
 
 //Reset the BT connection
@@ -172,13 +183,14 @@ void bt_reset()
 }
 
 
+
 //When a thumb key is pressed
 void thumbKeypadEvent(unsigned int kval, int state)
 {
   if(state != PRESSED && state != RELEASED)
     return;
     
-	//BUG: prevents NUM from being used as part of a chord
+  //BUG: prevents NUM from being used as part of a chord
   if(kval == NUM)
     return keypadEvent(kval, state);
   
@@ -198,16 +210,15 @@ void thumbKeypadEvent(unsigned int kval, int state)
 }
 
 
+
 //Called when keypad keys are pressed
 /* 
-	Chord logic: do nothing until a key is released. If it was the only
-	key, print its value. If more than one key was pressed:
-		- if the released key is not the last one (i.e., at least one key is
-			still held down), then it's a chord, so print it out
-		- if the released key was the last one of several, then the chord
-			value has already been printed out, so don't do anything else.
-		- BUG: Press A, B, C, then release C. Print value. Then release B.
-			Nothing should happen, but it will probably print again.
+  Chord logic: do nothing until a key is released. If it was the only
+  key, print its value. If more than one key was pressed:
+    - if the released key is not the last one (i.e., at least one key is
+      still held down), then it's a chord, so print it out
+    - if the released key was the last one of several, then the chord
+      value has already been printed out, so don't do anything else.
 */
 void keypadEvent(unsigned int kval, int state)
 {
@@ -215,28 +226,34 @@ void keypadEvent(unsigned int kval, int state)
     return;
     
   char *output_string;
- 
+  
   switch(state)
   {
     case PRESSED:
-      if(chord > 0)  //a key was already pressed
+      kprintf("Down: %d\n", kval);
+      if(keys_down > 0)  //a key was already pressed
         is_chord = 1;
-      chord |= kval;
+      keys_down |= kval;
+      output_chord = 0;
       break;
       
     case RELEASED:
-      output_string = get_keymap_string(chord);
-      chord ^= kval;
-      if(chord == 0)  //No more keys pressed
+      output_string = get_keymap_string(keys_down);
+      keys_down ^= kval;
+
+      kprintf("Up: %d; keys_down = %d; is_chord = %d\n", kval, keys_down, is_chord);
+      
+      if(keys_down == 0)
       {
-        if(!is_chord)  //This was not the last key of a chord, so print it
+        if(!is_chord)
           send_string(output_string);
+        output_chord = 0;
         is_chord = 0;
       }
-      else  //More keys held down, so print the current chord
+      else if(!output_chord)
       {
-        Serial.print(output_string);
         send_string(output_string);
+        output_chord = 1;
       }
       break;
 
@@ -246,15 +263,19 @@ void keypadEvent(unsigned int kval, int state)
 }
 
 
+
 void send_string(char *s)
 {
+  kprintf("send_string(%s)\n", s);
   for(int i = 0; i < strlen(s); i++)
     send_key(s[i]);
 }
 
 
+
 void send_key(char k)
 {
+  kprintf("send_key(%c)\n", k);
   //                0      1     2    3     4     5      6     7    8     9     10    11
   //                                       mod    0     kb0   kb1   kb2   kb3   kb4   kb5
   uint8_t seq[] = {0x9f, 0x0a, 0xa1, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
@@ -268,10 +289,15 @@ void send_key(char k)
     else
       seq[4] |= SHIFT;
   }
+
   for(int i = 0; i < 12; i++)
     Serial3.write(seq[i]);
+
+  //Modify seq to remvoe sent key and drop shift if it was there
   seq[6] = 0;
-  seq[4] ^= SHIFT;
+  if(seq[4] & SHIFT)
+    seq[4] ^= SHIFT;
+
   for(int i = 0; i < 12; i++)
     Serial3.write(seq[i]);
 }
